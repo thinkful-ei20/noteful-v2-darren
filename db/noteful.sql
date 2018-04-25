794643b35bd0d26e701d8f325a13c1d13af3a95c
Darren <ninenumbproductions@gmail.com>
2018-04-25 09:25:52 -0700

    added noteful.sql

---
+++ db/noteful.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS notes;
+
+CREATE TABLE notes(
+  id SERIAL PRIMARY KEY,
+  title text NOT NULL,
+  content text,
+  created timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER SEQUENCE notes_id_seq RESTART WITH 1001 INCREMENT BY 1;
+
+INSERT INTO notes (title,content)
+VALUES 
+('5 life lessons learned from cats','Lorem ipsum dolor sit amet, consectetur adipiscing elit,'),
+('What the government doesn''t want you to know about cats','Posuere sollicitudin aliquam'),
+('The most boring article about cats you''ll ever read', 'Lorem ipsum dolor sit amet,'),
+('7 things lady gaga has in common with cats','Tempor nec feugiat'),
+('The most incredible article about cats you''ll ever read','dolore magna aliqua'),
+('10 ways cats can help you live to 100','sollicitudin'),
+('9 reasons you can blame the recession on cats','voluptate velit'),
+('10 ways marketers are making you addicted to cats','Egestas egestas fringilla'),
+('11 ways investing in cats can make you a millionaire','ullamco laboris'),
+('Why you SHOULD forget everything you learned about cats','Tempor nec feugiat nisl pretium.');
+
