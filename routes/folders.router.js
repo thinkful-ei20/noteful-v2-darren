b37bc38cbfb1827eb599cecdb69a0b7c98577c13
Darren <ninenumbproductions@gmail.com>
2018-04-25 11:49:51 -0700

    routers mounted and set up via new folders.router.js

---
+++ routes/folders.router.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+
+const router = express.Router();
+
+const knex = require('../knex');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
