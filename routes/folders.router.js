795cf99e548f578724727547623858addfd3f230
Darren <ninenumbproductions@gmail.com>
2018-04-25 11:53:30 -0700

    added get folders all

--- routes/folders.router.js
+++ routes/folders.router.js
@@ -6,9 +6,14 @@ const router = express.Router();
 
 const knex = require('../knex');
 
-
-
-
+router.get('/folders', (req, res, next) => {
+  knex.select('id', 'name')
+    .from('folders')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => next(err));
+});
 
 
 
