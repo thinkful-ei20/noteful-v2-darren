914de9bbaaa40c03b2eb8a6b0fd96e209675c364
Darren <ninenumbproductions@gmail.com>
2018-04-25 11:36:39 -0700

    added folder_id

--- db/noteful.sql
+++ db/noteful.sql
@@ -4,25 +4,26 @@ CREATE TABLE notes(
   id SERIAL PRIMARY KEY,
   title text NOT NULL,
   content text,
-  created timestamp DEFAULT CURRENT_TIMESTAMP
+  created timestamp DEFAULT CURRENT_TIMESTAMP,
+  folder_id int REFERENCES folders ON DELETE SET NULL
 );
 
 ALTER SEQUENCE notes_id_seq RESTART WITH 1001 INCREMENT BY 1;
 
-INSERT INTO notes (title,content)
+INSERT INTO notes (title,content,folder_id)
 VALUES 
-('5 life lessons learned from cats','Lorem ipsum dolor sit amet, consectetur adipiscing elit,'),
-('What the government doesn''t want you to know about cats','Posuere sollicitudin aliquam'),
-('The most boring article about cats you''ll ever read', 'Lorem ipsum dolor sit amet,'),
-('7 things lady gaga has in common with cats','Tempor nec feugiat'),
-('The most incredible article about cats you''ll ever read','dolore magna aliqua'),
-('10 ways cats can help you live to 100','sollicitudin'),
-('9 reasons you can blame the recession on cats','voluptate velit'),
-('10 ways marketers are making you addicted to cats','Egestas egestas fringilla'),
-('11 ways investing in cats can make you a millionaire','ullamco laboris'),
-('Why you SHOULD forget everything you learned about cats','Tempor nec feugiat nisl pretium.');
+('5 life lessons learned from cats','Lorem ipsum dolor sit amet, consectetur adipiscing elit,',100),
+('What the government doesn''t want you to know about cats','Posuere sollicitudin aliquam',101),
+('The most boring article about cats you''ll ever read', 'Lorem ipsum dolor sit amet,', NULL),
+('7 things lady gaga has in common with cats','Tempor nec feugiat',102),
+('The most incredible article about cats you''ll ever read','dolore magna aliqua',NULL),
+('10 ways cats can help you live to 100','sollicitudin',103),
+('9 reasons you can blame the recession on cats','voluptate velit',NULL),
+('10 ways marketers are making you addicted to cats','Egestas egestas fringilla',102),
+('11 ways investing in cats can make you a millionaire','ullamco laboris',NULL),
+('Why you SHOULD forget everything you learned about cats','Tempor nec feugiat nisl pretium.',101);
 
-DROP TABLE IF EXISTS folders;
+DROP TABLE IF EXISTS folders CASCADE;
 
 CREATE TABLE folders (
   id SERIAL PRIMARY KEY,
