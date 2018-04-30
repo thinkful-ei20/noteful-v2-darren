272acf5aaa4b82424abe0dea0b2e865f755a4296
Darren <ninenumbproductions@gmail.com>
2018-04-29 20:30:07 -0700

    switched to travis option in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,35 @@
 'use strict';
 
+// LOCAL TESTING VERSION: 
+
+// module.exports = {
+//   development: {
+//     client: 'pg',
+//     connection: process.env.DATABASE_URL || 'postgres://dev:dev@localhost/noteful-app',
+//     debug: true, // http://knexjs.org/#Installation-debug
+//     pool: { min: 1, max: 2 }
+//   },
+//   production: {
+//     client: 'pg',
+//     connection: process.env.DATABASE_URL
+//   },
+//   test: {
+//     client: 'pg',
+//     connection: process.env.TEST_DATABASE_URL || 'postgres://dev:dev@localhost/noteful-test',
+//     pool: { min: 1, max: 2 }
+//   },
+// };
+
+
+
+
+
+// IF YOU WANT TO PUSH TO TRAVIS USE THIS ONE....
+
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://dev:dev@localhost/noteful-app',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/noteful-app',
     debug: true, // http://knexjs.org/#Installation-debug
     pool: { min: 1, max: 2 }
   },
@@ -13,7 +39,7 @@ module.exports = {
   },
   test: {
     client: 'pg',
-    connection: process.env.TEST_DATABASE_URL || 'postgres://dev:dev@localhost/noteful-test',
+    connection: process.env.TEST_DATABASE_URL || 'postgres://localhost/noteful-test',
     pool: { min: 1, max: 2 }
   },
 };
